3a150965aed51314ca5645b34555d00479f95e48
Paul Onyebuchi <70877999+ecoderP@users.noreply.github.com>
2024-05-04 00:41:27 +0100

    Plugin header

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+  * Plugin Name:        Plus WP Plugin
+  * Plugin URI:         https://paulonyebuchi.xyz 
+  * Description:        A plugin for adding blocks to a theme. 
+  * Version:            1.0 
+  * Requires at least:  5.9 
+  * Requires PHP:       7.2 
+  * Author:             Paul Onyebuchi
+  * Author URI:         https://paulonyebuchi.xyz   
+  * Text Domain:        Plus WP Plugin 
+  * Domain Path:        /languages 
+  * Requires Plugins:   my-plugin, yet-another-plugin 
+*/
+
+  if(!function_exists('add_action')) {
+    echo "You won't what you're looking for here"
+    exit;
+  }
